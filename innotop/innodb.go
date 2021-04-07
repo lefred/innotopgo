e75b209bffa3d4647a0a171ecf94490a8f4f1938
Frederic Descamps <lefred.descamps@gmail.com>
2021-04-07 17:24:52 +0200

    added information related to AHI removed the sleep(2) lol
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- innotop/innodb.go
+++ innotop/innodb.go
@@ -34,8 +34,61 @@ func refresh_innodb_info(ctx context.Context, interval time.Duration, fn func() 
 	}
 }
 
+func GetAHI(mydb *sql.DB) ([]string, [][]string, error) {
+	stmt := `SELECT ROUND(
+            (
+              SELECT Variable_value FROM sys.metrics
+              WHERE Variable_name = 'adaptive_hash_searches'
+            ) /
+            (
+              (
+               SELECT Variable_value FROM sys.metrics
+               WHERE Variable_name = 'adaptive_hash_searches_btree'
+              )  + (
+               SELECT Variable_value FROM sys.metrics
+               WHERE Variable_name = 'adaptive_hash_searches'
+              )
+            ) * 100,2
+          ) 'AHIRatio',
+		  ROUND(
+            (
+              SELECT Variable_value FROM sys.metrics
+              WHERE Variable_name = 'adaptive_hash_searches'
+            ) /
+            (
+              (
+               SELECT Variable_value FROM sys.metrics
+               WHERE Variable_name = 'adaptive_hash_searches_btree'
+              )  + (
+               SELECT Variable_value FROM sys.metrics
+               WHERE Variable_name = 'adaptive_hash_searches'
+              )
+            ) * 100
+          ) 'AHIRatioInt',
+		  (
+					SELECT variable_value
+					FROM performance_schema.global_variables
+					WHERE variable_name = 'innodb_adaptive_hash_index'
+		  ) AHIEnabled,
+		  (
+					SELECT variable_value
+					FROM performance_schema.global_variables
+					WHERE variable_name = 'innodb_adaptive_hash_index_parts'
+		  ) AHIParts
+	`
+	rows, err := db.Query(mydb, stmt)
+	if err != nil {
+		return nil, nil, err
+	}
+	cols, data, err := db.GetData(rows)
+	if err != nil {
+		return nil, nil, err
+	}
+	return cols, data, err
+}
+
 func GetBPFill(mydb *sql.DB) ([]string, [][]string, error) {
-	stmt := `SELECT sleep(2) as meh, ROUND(A.num * 100.0 / B.num)  BufferPoolFull, BP_Size, BP_instances
+	stmt := `SELECT ROUND(A.num * 100.0 / B.num)  BufferPoolFull, BP_Size, BP_instances
 	         FROM (
 				    SELECT variable_value num FROM performance_schema.global_status
 					WHERE variable_name = 'Innodb_buffer_pool_pages_data') A,
@@ -161,6 +214,15 @@ func DisplayInnoDB(mydb *sql.DB, c *container.Container, t *tcell.Terminal) (key
 		return k, err
 	}
 
+	ahi_graph, err := donut.New(
+		donut.CellOpts(cell.FgColor(cell.ColorGreen)),
+		donut.Label("AHI Ratio %", cell.FgColor(cell.ColorGreen)),
+	)
+	if err != nil {
+		cancel()
+		return k, err
+	}
+
 	top_window, err := text.New(text.WrapAtWords())
 	if err != nil {
 		cancel()
@@ -188,10 +250,22 @@ func DisplayInnoDB(mydb *sql.DB, c *container.Container, t *tcell.Terminal) (key
 				redo_info[cols[i]] = row[i]
 			}
 		}
+		cols, data, err = GetAHI(mydb)
+		if err != nil {
+			return err
+		}
+		var ahi_info = make(map[string]string)
+		for _, row := range data {
+			for i := 0; i < len(row); i++ {
+				ahi_info[cols[i]] = row[i]
+			}
+		}
 		graph_pct, _ := strconv.Atoi(bp_info["BufferPoolFull"])
 		bp_graph.Percent(graph_pct)
-		graph_pct, _ = strconv.Atoi(redo_info["CheckpointAgeInt"])
-		redo_graph.Percent(graph_pct)
+		chkpt_pct, _ := strconv.Atoi(redo_info["CheckpointAgeInt"])
+		redo_graph.Percent(chkpt_pct)
+		graph_pct, _ = strconv.Atoi(ahi_info["AHIRatioInt"])
+		ahi_graph.Percent(graph_pct)
 		uptime_sec, _ := strconv.Atoi(redo_info["Uptime"])
 		top_window.Reset()
 		top_window.Write(PrintLabel("Buffer Pool Size"))
@@ -213,57 +287,87 @@ func DisplayInnoDB(mydb *sql.DB, c *container.Container, t *tcell.Terminal) (key
 			top_window.Write(fmt.Sprintf("%-10v", redo_info["NbFiles"]))
 			top_window.Write("\n")
 			top_window.Write(PrintLabel("Checkpoint Info"))
-			top_window.Write(fmt.Sprintf("%-25v\n", redo_info["CheckpointInfo"]))
+			top_window.Write(fmt.Sprintf("%-25v", redo_info["CheckpointInfo"]))
 			top_window.Write("\n")
 			top_window.Write(PrintLabel("Checkpoint Age"))
 			color := cell.ColorDefault
-			if graph_pct > 80 {
+			if chkpt_pct > 80 {
 				color = cell.ColorRed
-			} else if graph_pct > 70 {
+			} else if chkpt_pct > 70 {
 				color = cell.ColorNumber(172)
 			}
-			top_window.Write(redo_info["CheckpointAge"]+"%\n", text.WriteCellOpts(cell.FgColor(color)))
+			top_window.Write(redo_info["CheckpointAge"]+"%", text.WriteCellOpts(cell.FgColor(color)))
 		}
+		top_window.Write("\n\n")
+		top_window.Write(PrintLabel("Adaptive Hash Index"))
+		top_window.Write(fmt.Sprintf("%-10v", ahi_info["AHIEnabled"]))
+		top_window.Write("\n")
+		if ahi_info["AHIEnabled"] == "ON" {
+			top_window.Write(PrintLabel("Num AHI Partitions"))
+			top_window.Write(fmt.Sprintf("%-10v", ahi_info["AHIParts"]))
+		}
+
 		return nil
 	})
 
 	c.Update("dyn_top_container",
-		container.SplitHorizontal(
-			container.Top(
-				container.SplitVertical(
-					container.Left(
+		container.SplitVertical(
+			container.Left(
+				container.SplitHorizontal(
+					container.Top(
 						container.Border(linestyle.Light),
 						container.ID("top_container"),
 						container.PlaceWidget(top_window),
 						container.FocusedColor(cell.ColorNumber(15)),
 					),
-					container.Right(
+					container.Bottom(
+						container.Border(linestyle.Light),
+						container.ID("main_container"),
+						container.PlaceWidget(details_window),
+						container.FocusedColor(cell.ColorNumber(15)),
+					),
+					container.SplitFixed(14),
+				),
+			),
+			container.Right(
+				container.SplitHorizontal(
+					container.Top(
 						container.SplitVertical(
 							container.Left(
 								container.Border(linestyle.Light),
-								container.ID("top_left_graph"),
+								container.ID("left_graph1"),
 								container.FocusedColor(cell.ColorNumber(15)),
 								container.PlaceWidget(redo_graph),
 							),
 							container.Right(
 								container.Border(linestyle.Light),
-								container.ID("top_right_graph"),
+								container.ID("right_graph1"),
 								container.FocusedColor(cell.ColorNumber(15)),
 								container.PlaceWidget(bp_graph),
 							),
 							container.SplitPercent(50),
 						),
 					),
-					container.SplitPercent(60),
+					container.Bottom(
+						container.SplitVertical(
+							container.Left(
+								container.Border(linestyle.Light),
+								container.ID("left_graph2"),
+								container.FocusedColor(cell.ColorNumber(15)),
+								container.PlaceWidget(ahi_graph),
+							),
+							container.Right(
+								container.Border(linestyle.Light),
+								container.ID("right_graph2"),
+								container.FocusedColor(cell.ColorNumber(15)),
+							),
+							container.SplitPercent(50),
+						),
+					),
+					container.SplitPercent(50),
 				),
 			),
-			container.Bottom(
-				container.Border(linestyle.Light),
-				container.ID("main_container"),
-				container.PlaceWidget(details_window),
-				container.FocusedColor(cell.ColorNumber(15)),
-			),
-			container.SplitFixed(12),
+			container.SplitFixed(85),
 		),
 	)
 	c.Update("bottom_container", container.Clear())
